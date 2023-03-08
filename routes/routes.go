23ada6b4c72195f9973dea7c9f1ffd7c9fe1f861
Elimists <pandey.pran@gmail.com>
2023-03-08 10:16:59 -0600

    Updated routes with Limiters

--- routes/routes.go
+++ routes/routes.go
@@ -13,9 +13,9 @@ func AllRoutes(app *fiber.App) {
 	version := "v2"
 	/*AUTH Routes*/
 	app.Post(fmt.Sprintf("/api/%s/verify", version), controller.VerifyEmail)
-	app.Post(fmt.Sprintf("/api/%s/register", version), controller.Register)
+	app.Post(fmt.Sprintf("/api/%s/register", version), middleware.Limiter(5, 40), controller.Register)
 	app.Post(fmt.Sprintf("/api/%s/login", version), middleware.Limiter(6, 45), controller.Login)
-	app.Post(fmt.Sprintf("/api/%s/resetpassword", version), controller.ResetPassword)
+	app.Post(fmt.Sprintf("/api/%s/resetpassword", version), middleware.Limiter(6, 45), controller.ResetPassword)
 
 	/*USER Routes*/
 	app.Get(fmt.Sprintf("api/%s/getuser", version), controller.GetUser)
@@ -32,5 +32,6 @@ func AllRoutes(app *fiber.App) {
 	app.Patch(fmt.Sprintf("/api/%s/updateuser", version), middleware.Protected(), middleware.Limiter(6, 60), controller.UpdateUser)
 	app.Post(fmt.Sprintf("api/%s/createdevice", version), middleware.Protected(), controller.AddDevice)
 	app.Patch(fmt.Sprintf("api/%s/uploadpic", version), middleware.Protected(), controller.UpdateProfilePic)
+	app.Post(fmt.Sprintf("/api/%s/updatepassword", version), middleware.Protected(), middleware.Limiter(6, 45), controller.UpdatePassword)
 
 }
